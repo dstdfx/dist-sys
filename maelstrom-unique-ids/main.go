dffd00d3a98de6d968710ccf7cad6d528106ba3d
Daniil Rutskiy <dstdfx@gmail.com>
2025-01-18 21:04:05 +0200

    Use random string and node id to generate unique id

--- maelstrom-unique-ids/main.go
+++ maelstrom-unique-ids/main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/base64"
 	"encoding/json"
-	"errors"
 	"fmt"
-
-	"github.com/google/uuid"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
 func main() {
 	n := maelstrom.NewNode()
+
 	n.Handle("generate", func(msg maelstrom.Message) error {
 		// Unmarshal the message body as an loosely-typed map.
 		var body map[string]any
@@ -19,19 +19,9 @@ func main() {
 			return err
 		}
 
-		nodeID := n.ID()
-		if nodeID == "" {
-			return errors.New("node ID not set")
-		}
-
-		uniqueID, err := uuid.NewRandom()
-		if err != nil {
-			return fmt.Errorf("failed to generate unique ID: %w", err)
-		}
-
 		// Build a unique ID by combining the generated UUID with the node ID.
 		responseBody := make(map[string]any)
-		responseBody["id"] = uniqueID.String() + "_" + nodeID
+		responseBody["id"] = fmt.Sprintf("%s-%s", generateRandomString(16), n.ID())
 		responseBody["type"] = "generate_ok"
 
 		return n.Reply(msg, responseBody)
@@ -40,4 +30,14 @@ func main() {
 	if err := n.Run(); err != nil {
 		panic(err)
 	}
+}
+
+func generateRandomString(length int) string {
+	bytes := make([]byte, length)
+	_, err := rand.Read(bytes)
+	if err != nil {
+		panic(err)
+	}
+
+	return base64.URLEncoding.EncodeToString(bytes)
 }
