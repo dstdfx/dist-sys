0feae57eb24db32e77bbd6ee228f4e391eb29d30
Daniil Rutskii <dstdfx@gmail.com>
2025-03-23 12:13:24 +0100

    Fix script fmt

--- ch6a-tx/run.sh
+++ ch6a-tx/run.sh
@@ -1,2 +1,11 @@
 go install .
-maelstrom test -w txn-rw-register --bin ~/go/bin/$(basename "$PWD") --node-count 1 --time-limit 20 --rate 1000 --concurrency 2n --consistency-models read-uncommitted --availability total
+
+maelstrom test \
+    -w txn-rw-register \
+    --bin ~/go/bin/$(basename "$PWD") \
+    --node-count 1 \
+    --time-limit 20 \
+    --rate 1000 \
+    --concurrency 2n \
+    --consistency-models read-uncommitted \
+    --availability total
